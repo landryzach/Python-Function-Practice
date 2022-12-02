76890cff3664db3037d353eb8ce0245398a1d9a8
Zachary Landry <landryzachary4@gmail.com>
2022-12-01 22:04:44 -0500

    mult list complete

--- func_prac.py
+++ func_prac.py
@@ -2,3 +2,16 @@ def max_num(a,b,c):
   return max([a,b,c])
 
 print(max_num(15,19,21))
+
+def mult_list(lst):
+    if len(lst) == 0:
+        return 0
+    prod = lst[0]
+
+    if len(lst) > 1:
+        for i in lst[1:]:
+            prod = prod * i
+
+    return prod
+        
+print(mult_list([4,17,37]))
